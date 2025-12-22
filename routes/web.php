36317cb890a2e0c072a7a81449b63e314eb6aa43
Muhtar Hidayat <hidayat.tm89@gmail.com>
2025-12-22 09:15:58 +0700

    Integrasi layouts and tempalting

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('customer.menu');
 });
+
+Route::get('/cart', function () {
+    return view('customer.cart');
+})->name('cart');
+
+Route::get('/checkout', function () {
+    return view('customer.checkout');
+})->name('checkout');
